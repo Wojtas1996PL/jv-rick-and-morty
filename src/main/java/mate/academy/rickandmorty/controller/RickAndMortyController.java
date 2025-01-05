c78fc510d45896cc2c922b97d24b19d9176498fc
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-01-05 15:08:55 +0100

    Second solution

--- src/main/java/mate/academy/rickandmorty/controller/RickAndMortyController.java
+++ src/main/java/mate/academy/rickandmorty/controller/RickAndMortyController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import mate.academy.rickandmorty.dto.CharacterDto;
 import mate.academy.rickandmorty.service.RickAndMortyClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,7 +24,7 @@ public class RickAndMortyController {
 
     @Operation(summary = "Get list of characters matching parameter")
     @GetMapping("/character")
-    public List<CharacterDto> getCharactersList(@PathVariable String character) {
+    public List<CharacterDto> getCharactersList(@RequestParam String character) {
         return rickAndMortyClient.getCharactersList(character);
     }
 }
--- src/main/java/mate/academy/rickandmorty/service/RickAndMortyClient.java
+++ src/main/java/mate/academy/rickandmorty/service/RickAndMortyClient.java
@@ -9,6 +9,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import mate.academy.rickandmorty.dto.CharacterDto;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,34 +21,56 @@ public class RickAndMortyClient {
     private static final int MAX = 826;
     private static final int MIN = 1;
     private static final int RANGE = MAX - MIN + 1;
+    private final SessionFactory sessionFactory;
+
+    public RickAndMortyClient(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     public CharacterDto getRandomCharacter() {
+        Transaction transaction = null;
         int random = (int) (Math.random() * RANGE) + MIN;
         HttpClient httpClient = HttpClient.newHttpClient();
         String url = BASE_URL + CHARACTER + "/" + random;
         HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
         try {
+            Session session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
             HttpResponse<String> httpResponse = httpClient
                     .send(httpRequest, HttpResponse.BodyHandlers.ofString());
             ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(httpResponse.body(), new TypeReference<>() {
-            });
+            CharacterDto characterDto = objectMapper.readValue(httpResponse.body(),
+                    new TypeReference<>() {});
+            session.persist(characterDto);
+            transaction.commit();
+            return characterDto;
         } catch (IOException | InterruptedException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException(e);
         }
     }
 
     public List<CharacterDto> getCharactersList(String character) {
+        Transaction transaction = null;
         HttpClient httpClient = HttpClient.newHttpClient();
-        String url = BASE_URL + CHARACTER + "/" + character;
+        String url = BASE_URL + CHARACTER + "?name=" + character;
         HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
         try {
+            Session session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
             HttpResponse<String> httpResponse = httpClient
                     .send(httpRequest, HttpResponse.BodyHandlers.ofString());
             ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(httpResponse.body(), new TypeReference<>() {
-            });
+            List<CharacterDto> characters = objectMapper
+                    .readValue(httpResponse.body(), new TypeReference<>() {});
+            transaction.commit();
+            return characters;
         } catch (IOException | InterruptedException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException(e);
         }
     }
